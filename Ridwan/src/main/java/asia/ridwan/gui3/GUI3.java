91ace9602b50c317278afebd28b23789fe9633d0
KaizoWan <156559854+KaizoWan@users.noreply.github.com>
2024-10-28 12:03:41 +0700

    Add files via upload

---
+++ Ridwan/src/main/java/asia/ridwan/gui3/GUI3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package asia.ridwan.gui3;
+
+/**
+ *
+ * @author ridwan
+ */
+public class GUI3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
